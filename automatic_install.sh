6e7680719f3ccb1d7a8c738b9937a3db75b560cd
Crazy <crazy@192.168.86.103>
2020-09-20 22:43:52 +0800

    初始化项目

--- automatic_install.sh
+++ automatic_install.sh
@@ -4,7 +4,7 @@ function AutomaticDepoly(){
     # 1. 杀死已经运行的线程
     ps -ef | grep hello-0.0.1-SNAPSHOT.jar | grep -v "grep" | cut -c 7-15 |xargs kill -9;
     # 2. git 拉取新的代码
-    git pull origin/master;
+    git pull;
     # 3. maven 打包新的程序
     mvn clean install -B ;
     # 4. 进入到 target 目录运行程序
